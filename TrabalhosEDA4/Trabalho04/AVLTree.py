325f8ba05674560d86357b1f16facdb90fd00452
Allan Nobre <allanjefrey9@hotmail.com>
2018-09-25 02:10:50 -0300

    Creating AVL Insertion Structure.
    Co-authored-by: Allan Nobre <allanjefrey9@hotmail.com>
    Co-authored-by: Filipe Barcelos <filipekn4@gmail.com>

---
+++ TrabalhosEDA4/Trabalho04/AVLTree.py
@@ -0,0 +1,155 @@
+rotationlog = []
+displaylog = []
+
+
+class Node():
+    def __init__(self, register):
+        self.key = int(register[0])
+        self.left = None
+        self.right = None
+        self.register = register
+
+
+class AVLTree():
+    def __init__(self, *args):
+        self.node = None
+        self.height = -1
+        self.balance = 0
+
+        if len(args) == 1:
+            for i in args[0]:
+                self.insert(i)
+
+    def height(self):
+        if self.node:
+            return self.node.height
+        else:
+            return 0
+
+    def is_leaf(self):
+        return (self.height == 0)
+
+    def insert(self, register):
+        tree = self.node
+
+        newnode = Node(register)
+
+        if tree is None:
+            self.node = newnode
+            self.node.left = AVLTree()
+            self.node.right = AVLTree()
+            # print("Inserted register [" + str(register) + "]")
+
+        elif int(register[0]) < tree.key:
+            self.node.left.insert(register)
+
+        elif int(register[0]) > tree.key:
+            self.node.right.insert(register)
+
+        else:
+            print("Key [" + str(register) + "] already in tree.")
+
+        self.rebalance()
+
+    def rebalance(self):
+        '''
+        Rebalance a particular (sub)tree
+        '''
+        # key inserted. Let's check if we're balanced
+        self.update_heights(False)
+        self.update_balances(False)
+        while self.balance < -1 or self.balance > 1:
+            if self.balance > 1:
+                if self.node.left.balance < 0:
+                    self.node.left.lrotate()  # we're in case II
+                    self.update_heights()
+                    self.update_balances()
+                self.rrotate()
+                self.update_heights()
+                self.update_balances()
+
+            if self.balance < -1:
+                if self.node.right.balance > 0:
+                    self.node.right.rrotate()  # we're in case III
+                    self.update_heights()
+                    self.update_balances()
+                self.lrotate()
+                self.update_heights()
+                self.update_balances()
+
+    def rrotate(self):
+        # Rotate left pivoting on self
+        rotationlog.append('Rotating the register: ' + str(self.node.register) + ' to right.')
+        print('Rotating ' + str(self.node.key) + ' right')
+        A = self.node
+        B = self.node.left.node
+        T = B.right.node
+
+        self.node = B
+        B.right.node = A
+        A.left.node = T
+
+    def lrotate(self):
+        # Rotate left pivoting on self
+        rotationlog.append('Rotating the register: ' + str(self.node.register) + ' to left.')
+        print('Rotating ' + str(self.node.key) + ' left')
+        A = self.node
+        B = self.node.right.node
+        T = B.left.node
+
+        self.node = B
+        B.left.node = A
+        A.right.node = T
+
+    def update_heights(self, recurse=True):
+        if self.node is not None:
+            if recurse:
+                if self.node.left is not None:
+                    self.node.left.update_heights()
+                if self.node.right is not None:
+                    self.node.right.update_heights()
+
+            self.height = max(self.node.left.height,
+                              self.node.right.height) + 1
+        else:
+            self.height = -1
+
+    def update_balances(self, recurse=True):
+        if self.node is not None:
+            if recurse:
+                if self.node.left is not None:
+                    self.node.left.update_balances()
+                if self.node.right is not None:
+                    self.node.right.update_balances()
+
+            self.balance = self.node.left.height - self.node.right.height
+        else:
+            self.balance = 0
+
+    def display(self, level=0, pref=''):
+        self.update_heights()  # Must update heights before balances
+        self.update_balances()
+        if self.node is not None:
+            displaylog.append("{} {} {} {} {}".format('-' * level * 2, pref, self.node.key, "[" + str(self.height) + ":" + str(self.balance) + "]", 'L' if self.is_leaf() else ' '))
+            print('-' * level * 2, pref, self.node.key, "[" + str(self.height) + ":" + str(self.balance) + "]", 'L' if self.is_leaf() else ' ')
+            if self.node.left is not None:
+                self.node.left.display(level + 1, '<')
+            if self.node.left is not None:
+                self.node.right.display(level + 1, '>')
+
+    def inorder_traverse(self):
+        if self.node is None:
+            return []
+
+        inlist = []
+        temp_list = self.node.left.inorder_traverse()
+        for i in temp_list:
+            inlist.append(i)
+
+        inlist.append(self.node.register)
+
+        temp_list = self.node.right.inorder_traverse()
+        for i in temp_list:
+            inlist.append(i)
+
+        return inlist
--- TrabalhosEDA4/Trabalho04/views.py
+++ TrabalhosEDA4/Trabalho04/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from Trabalho04.AVLTree import AVLTree, rotationlog, displaylog
 import time
 
 
@@ -6,36 +7,22 @@ def home(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         byte_str = myfile.file.read()
+
         # Convert to a "unicode" object
         text_obj = byte_str.decode('UTF-8')
         columns_descriptions, all_data = read_csv(text_obj.splitlines())
-        #
-        # print("ORDENAÇÃO selectionSort")
 
-        sorted_data, selection_sort_time = selectionSort(all_data)
+        register_tree = createAvlTree(all_data)
 
-        # # print("ORDENAÇÃO insertionSort")
-        #
-        sorted_data, insertion_sort_time = insertionSort(all_data)
+        register_tree.display()
+        print("IN MAIN")
+        print(rotationlog)
+        print(displaylog)
 
-        # # print("ORDENAÇÃO bubbleSort")
-        sorted_data, bubble_sort_time = bubbleSort(all_data)
-
-        # # print("ORDENAÇÃO shellSort")
-        sorted_data, shell_sort_time = shellSort(all_data)
-
-        sorted_data, count_sort_time = countSort(all_data)
-
-        sorted_data, radix_sort_time = radixSort(all_data)
+        sorted_data = register_tree.inorder_traverse()
 
         return render(request, 'result.html', {'columns_descriptions': columns_descriptions,
-                                               'sorted_data': sorted_data,
-                                               'selection_sort_time': selection_sort_time,
-                                               'insertion_sort_time': insertion_sort_time,
-                                               'bubble_sort_time': bubble_sort_time,
-                                               'shell_sort_time': shell_sort_time,
-                                               'count_sort_time': count_sort_time,
-                                               'radix_sort_time': radix_sort_time})
+                                               'sorted_data': sorted_data})
     else:
         # Nothing to do
         pass
@@ -60,122 +47,13 @@ def read_csv(file):
     return columns_descriptions, all_data
 
 
-def selectionSort(dataset):
-    sorted_data = list(dataset)
-    time_initial = time.time()
-    for currently_checked_position in range(len(sorted_data)):
-        lower_position = currently_checked_position
+def createAvlTree(dataset):
+    register_tree = AVLTree()
 
-        for position_searched in range(currently_checked_position+1, len(sorted_data)):
-            if int(sorted_data[lower_position][0]) > int(sorted_data[position_searched][0]):
-                lower_position = position_searched
+    for register in dataset:
+        register_tree.insert(register)
 
-        temporary_register = sorted_data[currently_checked_position]
-        sorted_data[currently_checked_position] = sorted_data[lower_position]
-        sorted_data[lower_position] = temporary_register
-
-    time_final = time.time() - time_initial
-    return sorted_data, time_final
-
-
-def insertionSort(dataset):
-    sorted_data = list(dataset)
-    time_initial = time.time()
-    for currently_checked_position in range(1, len(sorted_data)):
-        currently_checked_data = sorted_data[currently_checked_position]
-
-        position_searched = currently_checked_position - 1
-
-        while (position_searched > -1) and int(currently_checked_data[0]) < int(sorted_data[position_searched][0]):
-            sorted_data[position_searched + 1] = sorted_data[position_searched]
-            position_searched = position_searched - 1
-
-        sorted_data[position_searched + 1] = currently_checked_data
-
-    time_final = time.time() - time_initial
-    return sorted_data, time_final
-
-
-def bubbleSort(dataset):
-    sorted_data = list(dataset)
-    time_initial = time.time()
-    final_position_to_be_checked = len(sorted_data) - 1
-    occurred_swap = True
-
-    while (final_position_to_be_checked > 0) and occurred_swap:
-        occurred_swap = False
-
-        for currently_checked_position in range(final_position_to_be_checked):
-            if int(sorted_data[currently_checked_position][0]) > int(sorted_data[currently_checked_position + 1][0]):
-                occurred_swap = True
-                temporary = sorted_data[currently_checked_position]
-                sorted_data[currently_checked_position] = sorted_data[currently_checked_position+1]
-                sorted_data[currently_checked_position+1] = temporary
-
-        final_position_to_be_checked = final_position_to_be_checked - 1
-
-    time_final = time.time() - time_initial
-    return sorted_data, time_final
-
-
-def shellSort(dataset):
-    sorted_data = list(dataset)
-    time_initial = time.time()
-
-    dataset_length = len(sorted_data)
-    gap = int(dataset_length / 2)
-    while gap > 0:
-            for num in range(gap, dataset_length):
-                data = sorted_data[num]
-                position = num
-                while position >= gap and int(data[0]) < int(sorted_data[position - gap][0]):
-                    sorted_data[position] = sorted_data[position - gap]
-                    position = position - gap
-                    sorted_data[position] = data
-            gap = int(gap / 2)
-
-    time_final = time.time() - time_initial
-    return sorted_data, time_final
-
-
-def countSort(dataset):
-    sorted_list = list(dataset)
-    time_initial = time.time()
-
-    # The sorted_list character array that will have sorted arr
-    output_list = [0 for position in range(max([int(item[0]) for item in dataset]) + 2)]
-    print(max([int(item[0]) for item in dataset]) + 2)
-
-    # Create a count array to store count of inidividul
-    # characters and initialize count array as 0
-    count_list = [0 for position in range(max([int(item[0]) for item in dataset]) + 2)]
-
-    # For storing the resulting answer since the
-    # string is immutable
-    sorted_list = [0 for data in dataset]
-
-    # Store count_list of each character
-    for data in dataset:
-        number = int(data[0])
-        count_list[number] += 1
-
-    # Change count_list[i] so that count_list[i] now contains actual
-    # position of this character in output_list array
-    for position in range(max([int(item[0]) for item in dataset]) + 2):
-        count_list[position] += count_list[position-1]
-
-    # Build the output_list character array
-    for position in range(len(dataset)):
-        output_list[count_list[int(dataset[position][0])]-1] = dataset[position]
-        count_list[int(dataset[position][0])] -= 1
-
-    # Copy the output_list array to arr, so that arr now
-    # contains sorted characters
-    for position in range(len(dataset)):
-        sorted_list[position] = output_list[position]
-
-    time_final = time.time() - time_initial
-    return sorted_list, time_final
+    return register_tree
 
 
 def radixSort(dataset):
